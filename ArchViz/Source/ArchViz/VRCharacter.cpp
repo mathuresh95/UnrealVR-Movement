489930d64ff83a1923d4b52bf2f7e6abca3fa47b
Mathureswaran Rajagopal <mathuresh95@gmail.com>
2019-04-30 06:07:47 +0530

    Basic Player Movement with Blinkers. Still requires polish

--- ArchViz/Source/ArchViz/VRCharacter.cpp
+++ ArchViz/Source/ArchViz/VRCharacter.cpp
@@ -1,14 +1,24 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+
+
+// Mostly copied and pasted by Mathureswaran
 
 #include "VRCharacter.h"
 #include"Components/InputComponent.h"
 #include"Components/StaticMeshComponent.h"
+#include "GameFramework/Actor.h"
+#include"Engine/Classes/Camera/PlayerCameraManager.h"
+#include"Engine/Classes/GameFramework/PlayerController.h"
+#include"Engine/Public/TimerManager.h"
+#include"Components/CapsuleComponent.h"
+#include"NavigationSystem.h"
+#include"Components/PostProcessComponent.h"
+#include "Materials/MaterialInstanceDynamic.h"
 #include"DrawDebugHelpers.h"
 
 // Sets default values
 AVRCharacter::AVRCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	VRRoot = CreateDefaultSubobject<USceneComponent>(FName("VRRoot"));
@@ -19,13 +29,21 @@ AVRCharacter::AVRCharacter()
 
 	DestinationMarker = CreateDefaultSubobject<UStaticMeshComponent>(FName("Destination Marker"));
 	DestinationMarker->SetupAttachment(GetRootComponent());
+
+	PostProcessComponent = CreateDefaultSubobject<UPostProcessComponent>(FName("PostProcessComponent"));
+	PostProcessComponent->SetupAttachment(GetRootComponent());
 }
 
 // Called when the game starts or when spawned
 void AVRCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	if (BlinkerMaterialBase)
+	{
+		BlinkerMaterialInstance =  UMaterialInstanceDynamic::Create(BlinkerMaterialBase, this);
+		PostProcessComponent->AddOrUpdateBlendable(BlinkerMaterialInstance);
+	}
+
 }
 
 // Called every frame
@@ -38,33 +56,144 @@ void AVRCharacter::Tick(float DeltaTime)
 	NewCameraOffset.Z = 0;
 	AddActorWorldOffset(NewCameraOffset);
 	VRRoot->AddWorldOffset(-NewCameraOffset);
-	
-	UpdateDestinationMarker();
 
-	
+	UpdateDestinationMarker();
+	UpdateBlinkers();
+
+
 }
 
 // Called to bind functionality to input
 void AVRCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-	PlayerInputComponent->BindAxis(FName("Forward"), this, &AVRCharacter::MoveForward);
-	PlayerInputComponent->BindAxis(FName("Right"), this, &AVRCharacter::MoveRight);
+	PlayerInputComponent->BindAxis(TEXT("Forward"), this, &AVRCharacter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("Right"), this, &AVRCharacter::MoveRight);
+	PlayerInputComponent->BindAction(TEXT("Teleport"), IE_Released, this, &AVRCharacter::BeginTeleport);
+	//PlayerInputComponent->BindAction(TEXT("Teleport"), this, &AVRCharacter::BeginTeleport);
 
+}
+
+bool AVRCharacter::FindTeleportDestination(FVector & OutLocation)
+{
+	FHitResult HitResult;
+	FVector Start = Camera->GetComponentLocation();
+	FVector End = Start + Camera->GetForwardVector()*MaxTeleportRange;
+
+	bool bHit = GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, ECC_Visibility);
+
+	if (!bHit)
+	{
+		return false;
+	}
+	FNavLocation NavLocation;
+	bool bOnNavMesh = UNavigationSystemV1::GetNavigationSystem(GetWorld())->ProjectPointToNavigation(HitResult.Location, NavLocation, TeleportProjectionExtent);
+
+	if (!bOnNavMesh)
+	{
+		return false;
+	}
+	/*if (bOnNavMesh && bHit)
+	{
+
+	}
+
+	else
+	{
+		DestinationMarker->SetVisibility(false);
+	}*/
+	DrawDebugLine(GetWorld(), Start, End, FColor(1, 0, 0), true, 1.0f, 1, 1.0f);
+
+	OutLocation = NavLocation.Location;
+	return true;
 }
 
 void AVRCharacter::UpdateDestinationMarker()
 {
-	FHitResult HitResult;
-	FVector Start= Camera->GetComponentLocation() ;
-	FVector End = Start + Camera->GetForwardVector()*MaxTeleportRange;
-
-	bool bHit =  GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, ECC_Visibility);
-	if (bHit)
+	FVector Location;
+	bool bHasDestination = FindTeleportDestination(Location);
+	if (bHasDestination)
 	{
-		DestinationMarker->SetWorldLocation(HitResult.Location);
+		DestinationMarker->SetWorldLocation(Location);
+		DestinationMarker->SetVisibility(true);
 	}
-	DrawDebugLine(GetWorld(), Start, End, FColor(1, 0, 0), true, 1.0f, 1, 1.0f);
+	else
+	{
+		DestinationMarker->SetVisibility(false);
+	}
+
+
+
+}
+void AVRCharacter::UpdateBlinkers()
+{
+	if (RadiusVsVelocity == nullptr)
+	{
+		return;
+	}
+	float Speed = GetVelocity().Size();
+	float Radius = RadiusVsVelocity->GetFloatValue(Speed);
+
+	BlinkerMaterialInstance->SetScalarParameterValue(TEXT("Radius"), Radius);
+	FVector2D Center = GetBlinkerCenter();
+	//BlinkerMaterialInstance->SetVectorParameterValue(TEXT("Center"), FLinearColor(Center.X,Center.Y,0));
+	//BlinkerMaterialInstance->SetScalarParameterValue(TEXT("Radius"),Radius);
+}
+
+void AVRCharacter::BeginTeleport()
+{
+	APlayerController* PC = Cast<APlayerController>(GetController());
+
+	if (PC != nullptr)
+	{
+		StartFade(0, 1);
+	}
+	FTimerHandle Handle;
+	GetWorldTimerManager().SetTimer(Handle, this, &AVRCharacter::EndTeleport, FadeTime);
+
+}
+
+void AVRCharacter::EndTeleport()
+{
+	SetActorLocation(DestinationMarker->GetComponentLocation());
+	StartFade(1, 0);
+}
+
+void AVRCharacter::StartFade(float FromAlpha, float ToAlpha)
+{
+	APlayerController* PC = Cast<APlayerController>(GetController());
+	if (PC != nullptr)
+	{
+		PC->PlayerCameraManager->StartCameraFade(FromAlpha, ToAlpha, FadeTime, FLinearColor::Black);
+	}
+}
+
+
+FVector2D AVRCharacter::GetBlinkerCenter()
+{
+	APlayerController* PlayerController = Cast<APlayerController>(GetController());
+	if (!PlayerController)
+	{
+		return FVector2D(0.5f, 0.5f);
+	}
+	FVector MovementDirection = GetVelocity().GetSafeNormal();
+	if (MovementDirection.IsNearlyZero())
+	{
+		return FVector2D(0.5, 0.5);
+	}
+
+	FVector WorldStationaryLocation = Camera->GetComponentLocation() + MovementDirection * 1000.0f;
+	FVector2D ScreenStationaryLocation;
+
+	PlayerController->ProjectWorldLocationToScreen(WorldStationaryLocation,ScreenStationaryLocation);
+
+	int SizeX, SizeY;
+
+	PlayerController->GetViewportSize(SizeX,SizeY);
+	ScreenStationaryLocation.X = ScreenStationaryLocation.X / SizeX;
+	ScreenStationaryLocation.Y = ScreenStationaryLocation.Y / SizeY;
+
+	return ScreenStationaryLocation;
 }
 
 void AVRCharacter::MoveForward(float Throttle)
--- ArchViz/Source/ArchViz/VRCharacter.h
+++ ArchViz/Source/ArchViz/VRCharacter.h
@@ -27,18 +27,40 @@ public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 private:
+	bool FindTeleportDestination(FVector &OutLocation);
 	void UpdateDestinationMarker();
+	void BeginTeleport();
+	void EndTeleport();
+	void StartFade(float FromAlpha, float ToAplha);
+	void UpdateBlinkers();
+	FVector2D GetBlinkerCenter();
 	void MoveForward(float Throttle);
 	void MoveRight(float Throttle);
 
-private:
-	UPROPERTY(VisibleAnywhere,Category = Setup)
-	class UCameraComponent* Camera;
 	UPROPERTY(VisibleAnywhere, Category = Setup)
-	class USceneComponent* VRRoot;
+		class UCameraComponent* Camera;
 	UPROPERTY(VisibleAnywhere, Category = Setup)
-	class UStaticMeshComponent* DestinationMarker;
+		class USceneComponent* VRRoot;
+	UPROPERTY(VisibleAnywhere, Category = Setup)
+		class UStaticMeshComponent* DestinationMarker;
+	UPROPERTY(VisibleAnywhere, Category = Setup)
+		class UPostProcessComponent* PostProcessComponent;
 	UPROPERTY(EditAnywhere, Category = Setup)
-		float MaxTeleportRange = 4000.0f;
+		class UMaterialInterface* BlinkerMaterialBase;
+	UPROPERTY(EditAnywhere, Category = Setup)
+		class UMaterialInstanceDynamic* BlinkerMaterialInstance;
+	UPROPERTY(EditAnywhere, Category = Setup)
+		class UCurveFloat* RadiusVsVelocity;
+	
+		
+
+private:
+	UPROPERTY(EditAnywhere, Category = Setup)
+		float MaxTeleportRange = 500.0f;
+	UPROPERTY(EditAnywhere, Category = Setup)
+		float FadeTime = 2.0f;
+	UPROPERTY(EditAnywhere, Category = Setup)
+		FVector TeleportProjectionExtent = FVector(100, 100, 100);
+
 
 };
